af7fd54489ddd4dad1c7ef4a93fb0a9e87092182
IDontDoCode <letticastor@gmail.com>
2023-01-15 14:04:02 +0100

    update

--- src/components/displayPane/components/ButtomButtons.tsx
+++ src/components/displayPane/components/ButtomButtons.tsx
@@ -1,8 +1,8 @@
-import "./ButtomButtons.css";
-
 import { AiFillHome } from "react-icons/ai";
 import { BsCashCoin } from "react-icons/bs";
 import { GiFarmTractor } from "react-icons/gi";
+
+import "./ButtomButtons.css";
 
 interface HeaderButtonsProps {
   currentView: string;
@@ -13,23 +13,26 @@ const ButtomButtons: React.FC<HeaderButtonsProps> = ({ setCurrentView }) => {
   return (
     <div
       style={{
-        position: "absolute",
+        position: "fixed",
         bottom: "0",
         width: "100%",
         display: "flex",
         justifyContent: "space-around",
         backgroundColor: "#333333",
-        paddingTop: "15px"
+        paddingTop: "8px"
       }}
     >
-      <div style={{ color: "white", fontSize: "40px" }} onClick={() => setCurrentView("home")}>
+      <div className="home-button" style={{ color: "white", fontSize: "30px" }} onClick={() => setCurrentView("home")}>
         <AiFillHome />
+        <p style={{ fontSize: "13px", marginTop: "-5px" }}>Home</p>
       </div>
-      <div style={{ color: "White", fontSize: "40px" }} onClick={() => setCurrentView("pools")}>
+      <div className="home-button" style={{ color: "white", fontSize: "30px" }} onClick={() => setCurrentView("pools")}>
         <BsCashCoin />
+        <p style={{ fontSize: "13px", marginTop: "-5px" }}>Pools</p>
       </div>
-      <div style={{ color: "White", fontSize: "40px" }} onClick={() => setCurrentView("farms")}>
+      <div className="home-button" style={{ color: "white", fontSize: "30px" }} onClick={() => setCurrentView("farms")}>
         <GiFarmTractor />
+        <p style={{ fontSize: "13px", marginTop: "-5px" }}>Farms</p>
       </div>
     </div>
   );
