6a113f73f430f746a6b622da5c45e620eb94ec58
IDontDoCode <letticastor@gmail.com>
2023-01-19 20:05:46 +0100

    update pools

--- src/components/displayPane/Pools.tsx
+++ src/components/displayPane/Pools.tsx
@@ -1,55 +1,48 @@
-import { Divider } from "antd";
+
 
 import { useWindowWidthAndHeight } from "../../hooks/useWindowWidthAndHeight";
 import StakeEVaultCard from "./components/StakeEVaultCard";
-import "./Pools.css";
 
-const styles = {
-  container: {
-    background: "#f5f4f4",
-    width: "80%",
-    minWidth: "34px",
-    maxWidth: "900px",
-    textAlign: "center",
-    margin: "auto",
-    padding: "30px 0",
-    borderRadius: "20px"
-  },
-  title: {
-    color: "black",
-    fontWeight: 600,
-    fontSize: "30px",
-    marginBottom: "10px"
-  },
-  content: {
-    width: "85%",
-    margin: "auto",
-    fontSize: "17px"
-  },
-  action: {
-    display: "inline-flex",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    gap: "20px"
-  },
-  display: {
-    paddingBlock: "15px"
-  },
-  statusText: {
-    color: "rgb(46, 46, 46)",
-    fontWeight: 800
-  },
-  card: {
-    maxWidth: "100%"
-  }
-} as const;
+import "./Pools.css";
 
 const Pools: React.FC = () => {
   const [width] = useWindowWidthAndHeight();
-  const isMobile = width <= 768;
+  const styles = {
+    container: {
+      background: "#f5f4f4",
+      width: width <= 768 ? "100%" : "80%",
+      minWidth: "400px",
+      maxWidth: "900px",
+      textAlign: "center",
+      margin: "auto",
+      padding: "30px 0",
+      borderRadius: "20px"
+    },
+    title: {
+      color: "black",
+      fontWeight: 600,
+      fontSize: "30px",
+      marginBottom: "10px"
+    },
+    content: {
+      width: "85%",
+      margin: "auto",
+      fontSize: "17px"
+    },
+    display: {
+      paddingBlock: "15px"
+    },
+    statusText: {
+      color: "rgb(46, 46, 46)",
+      fontWeight: 800
+    },
+    card: {
+      maxWidth: "100%"
+    }
+  } as const;
 
   return (
-    <div style={styles.container}>
+    <div style={{ ...styles.container}}>
       <div style={styles.title}>Pools</div>
       <div style={styles.content}>
         All active pools, send me a dm if you want your token here
@@ -57,18 +50,13 @@ const Pools: React.FC = () => {
           https://t.me/Aleksandee
         </a>
       </div>
+      
       <div style={styles.content}>
-        <>
-          <Divider />
           <div style={{ display: "block" }}>
             <div className="card">
               <StakeEVaultCard />
             </div>
           </div>
-          <div style={styles.action}>
-            {!isMobile && <Divider type="vertical" style={{ fontSize: "120px !important" }} />}
-          </div>
-        </>
       </div>
     </div>
   );
--- src/components/displayPane/components/StakeEVaultCard.tsx
+++ src/components/displayPane/components/StakeEVaultCard.tsx
@@ -1,31 +1,67 @@
 import { useState } from "react";
-import "./StakeEVaultCard.css";
 import { BiArrowFromTop } from "react-icons/bi";
 import { BiArrowToTop } from "react-icons/bi";
+import logo from "../components/Logos/0xeE34Af939a75223571ac818f0958b67cba48cd01.png";
 
 const StakeEVaultCard = () => {
-const [expanded, setExpanded] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
-return (
-<div
-style={{
-height: expanded ? "150px" : "75px",
-background: "white",
-borderRadius: "5px",
-border: "1px solid black",
-display: "flex",
-position: "relative"
-}}
-onClick={() => setExpanded(!expanded)}
->
-<div style={{ position: "absolute", bottom: -20, right: 30, padding: "10px" ,paddingRight: "30 px", fontSize: "30px" }}>
-{!expanded ? <BiArrowFromTop /> : <BiArrowToTop />}
-</div>
-<div style={{ position: "absolute", bottom: "30px", right: "10px" }}>
+  return (
+    <div
+      style={{
+        height: expanded ? "150px" : "75px",
+        background: "white",
+        borderRadius: "5px",
+        border: "1px solid black",
+        display: "flex",
+        position: "relative",
+      }}
+      onClick={() => setExpanded(!expanded)}
+    >
+      <div
+        style={{
+          position: "absolute",
+          bottom: -20,
+          right: 30,
+          padding: "10px",
+          paddingRight: "30 px",
+          fontSize: "30px",
+        }}
+      >
+        {!expanded ? <BiArrowFromTop /> : <BiArrowToTop />}
+      </div>
+      <div style={{ position: "absolute", bottom: "30px", right: "20px" }}>
 {!expanded ? "More Info!" : "Less Info!"}
 </div>
-</div>
-);
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <img src={logo} alt="Logo" style={{ height: "60px",paddingLeft: "10px",paddingTop: "10px" }} />
+        <div style={{ marginLeft: "10px" }}>
+          <div style={{ fontWeight: "bold" }}>Manual eVault</div>
+          <div>Earn eVault,</div>
+          <div>Stake eVault</div>
+        </div>
+
+        <div
+    style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    }}
+  >
+          <div>APR 10%</div>
+        </div>
+
+      </div>
+    </div>
+  );
 };
 
 export default StakeEVaultCard;
