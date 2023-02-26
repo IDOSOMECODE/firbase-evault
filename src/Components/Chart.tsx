918a0e60b5df8ccb5a915679c189321a25e31ecc
IDontDoCode <letticastor@gmail.com>
2023-02-26 22:10:10 +0100

    ok

--- src/Components/Chart.tsx
+++ src/Components/Chart.tsx
@@ -1,59 +1,50 @@
 import { useWindowWidthAndHeight } from "../Hooks/useWindowWidthAndHeight";
 import React from "react";
+import { AdvancedRealTimeChart } from "react-ts-tradingview-widgets";
 
-const SwapBox: React.FC = () => {
+const Chart: React.FC = () => {
   const [width] = useWindowWidthAndHeight();
 
-const styles = {
-container: {
-background: "#f5f4f4",
-width: "100%",
-textAlign: "center",
-borderRadius: "20px",
-padding: "10px 0",
-display: "flex",
-flexDirection: "row",
-marginTop: "5px",
-marginLeft: width < 820 ? 0 : "5px",
-marginRight: width < 820 ? 0 : "5px"
-},
-content: {
-fontSize: "11px",
-display: "inline-block",
-width: "100px",
-marginRight: "20px"
-},
-imageContainer: {
-width: "55px",
-height: "55px",
-display: "flex",
-alignItems: "center",
-justifyContent: "center"
-},
-image: {
-objectFit: "contain",
-PaddingRight: "10px",
-paddingLeft: "10px"
-},
-content2: {
-fontSize: "11px",
-display: "flex",
-alignItems: "center",
-justifyContent: "center",
-width: "100px",
-marginRight: "20px"
-}
-} as const;
+  const styles = {
+    container: {
+      background: "#f5f4f4",
+      width: "100%",
+      textAlign: "center",
+      borderRadius: "20px",
+      padding: "10px 0",
+      display: "flex",
+      flexDirection: "row",
+      marginTop: "5px",
+      marginLeft: width < 820 ? 0 : "5px",
+      marginRight: width < 820 ? 0 : "5px"
+    },
+    content: {
+      fontSize: "11px",
+      display: "inline-block",
+      width: "100px",
+      marginRight: "20px"
+    },
+    image: {
+      objectFit: "contain",
+      PaddingRight: "10px",
+      paddingLeft: "10px"
+    },
+    content2: {
+      fontSize: "11px",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      width: "100px",
+      marginRight: "20px"
+    }
+  } as const;
 
-return (
-<div style={styles.container}>
-<div style={styles.imageContainer}>
-
-      </div>
-      
+  return (
+    <div style={styles.container}>
+    
+ 
     </div>
   );
 };
 
-
-    export default SwapBox;
+export default Chart;
